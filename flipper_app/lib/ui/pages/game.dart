0d5d8aa41430a16753961356f97da5232fc9e85d
Festus <festusowumi@gmail.com>
2020-06-02 18:31:37 +0100

    game room looks nice, implemented timer feature on the top

--- flipper_app/lib/ui/pages/game.dart
+++ flipper_app/lib/ui/pages/game.dart
@@ -1,3 +1,4 @@
+import 'package:flipper_app/ui/widgets/timer.dart';
 import 'package:flutter/material.dart';
 import 'package:flipper_app/ui/widgets/flipper.dart';
 
@@ -17,19 +18,33 @@ class _GameRoomState extends State<GameRoom> {
           children: <Widget>[
             Expanded(
               flex: 1,
-              child: Container(child: Placeholder(color: Colors.black)),
+              child: Container(
+                  child: Container(
+                width: 100.0,
+                padding: EdgeInsets.only(top: 10.0, right: 4.0),
+                child: CountDownTimer(
+                  secondsRemaining: 59,
+                  whenTimeExpires: () {
+                    setState(() {
+                      // hasTimerStopped = true;
+                    });
+                  },
+                  countDownTimerStyle: TextStyle(
+                      color: Colors.black, fontSize: 25.0, height: 1.2),
+                ),
+              )),
             ),
             SizedBox(height: appHeight / 20),
             Expanded(
               flex: 10,
               child: Container(
                   child: GridView.builder(
+                    padding: EdgeInsets.only(left: 5.0, right: 5.0),
                       itemCount: 12,
                       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                           crossAxisCount: 3,
                           mainAxisSpacing: 30.0,
-                          crossAxisSpacing: 10.0
-                          ),
+                          crossAxisSpacing: 10.0),
                       itemBuilder: (BuildContext context, int index) {
                         return FlipperWidget();
                       })),
